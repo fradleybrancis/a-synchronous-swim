40e7d6a9c828734c750443f8bee50f23f2e6d73b
fradleybrancis <fradleybrancis@gmail.com>
2019-03-08 20:28:27 -0800

    Implement partial keypress solution

--- client/js/httpHandler.js
+++ client/js/httpHandler.js
@@ -1,32 +1,18 @@
 (function() {
 
-  debugger;
   const serverUrl = 'http://127.0.0.1:3000';
 
-  setInterval(() => {
+ setInterval(() => {
 
     $.ajax({
       method: 'GET',
       url: serverUrl,
       success: (data) => {
-        console.log(data);
         SwimTeam.move(data.toString());
       },
       error: () => (console.log('Failed GET Request'))
 
     })
-
-    /* Pseudo Code
-
-    Prepare + Send AJAX Request to Server
-      use serverURL
-      determine callback
-
-
-
-    Note: Need tests of output!
-    */
-
   }, 100);
 
 })();
